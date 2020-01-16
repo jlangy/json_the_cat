d7344a4db521f0333660a3bfadcd42e3dcaebfaa
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-16 20:21:07 +0000

    print out one cats description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,5 @@
 const request = require('request');
 
 const catBreed = request('https://api.thecatapi.com/v1/breeds/search?q=sib', (err, response, body) => {
-  console.log(err, response, body);
+  console.log(JSON.parse(body)[0].description);
 });
