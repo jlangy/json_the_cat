48e69c4f62f8cd297a42b4a592a698841eaf899b
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-16 20:25:54 +0000

    logs a single breedquery fro command line

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,8 @@
 const request = require('request');
+const breedQuery = process.argv.slice(2,3);
 
-const catBreed = request('https://api.thecatapi.com/v1/breeds/search?q=sib', (err, response, body) => {
+const catBreed = request(`https://api.thecatapi.com/v1/breeds/search?q=${breedQuery}`, (err, response, body) => {
+  if(err) throw(err);
+
   console.log(JSON.parse(body)[0].description);
 });
